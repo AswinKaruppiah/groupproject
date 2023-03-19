f44c04878879694f03ec723cda28ed2474b03e41
AswinKaruppiah <aswinthkaruppiah@gmail.com>
2023-03-19 13:26:02 +0530

    text change

--- src/UI/Display/Products/Product.js
+++ src/UI/Display/Products/Product.js
@@ -5,7 +5,7 @@ function Product() {
   return (
     <div className="product">
       <div className="info">
-        <h1>Rawin</h1>
+        <h1>aswin</h1>
         <h1> Sample Text </h1>
       </div>
     </div>
